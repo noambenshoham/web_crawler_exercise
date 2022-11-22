ce1fcc0efb046e47f35d550630404cc6236a4ac2
noambenshoham <noambenshoham@cyber4s.dev>
2022-11-23 01:06:47 +0200

    Code works

--- web_crawler.py
+++ web_crawler.py
@@ -4,50 +4,50 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import validators
-# import cssutils
 
 result_file_path = "results.json"
 
 def main():
     main_url = sys.argv[1]
     depth_string = sys.argv[2]
-    results = get_results(main_url, int(depth_string))
-    print(results)
+    results = get_results(main_url, 0, int(depth_string))
+ 
     results_json = {"results": results}
     result_file_path = Path("results.json")
     results_json_string = json.dumps(results_json)
     result_file_path.write_text(results_json_string)
 
-def get_results(url, maxDepth):
+def get_results(url, depth, max_depth):
     results = []
-    for depth in range(maxDepth + 1):
+    try:
         html_string = get_web_page(url)
-        images_links = get_image_srcs(html_string, url)
-        for image in images_links:
-            results.append({
-                "imageUrl": image,
-                "sourceUrl": url,
-                "depth": depth,
-            })
-        
-        for url in get_a_hrefs(html_string):
-            if (url[:4] == 'http') & (depth < maxDepth):
-                depth_results = get_results(url, depth)
-                results += depth_results
-    print(results)
+        images_srcs = get_image_srcs(html_string, url)
+    except:
+        return []
+    for image_src in images_srcs:
+        results.append({
+            "imageUrl": image_src,
+            "sourceUrl": url,
+            "depth": depth,
+        })
+    if depth == max_depth:
+        return results
+    links = get_a_hrefs(html_string)
+    for link in links:
+        results += get_results(link, depth + 1, max_depth)
     return results
 
 def get_web_page(page_link):
     response = requests.get(page_link)
     file_content = response.text
     return file_content
-
 def get_image_srcs(html_string, url):
+
     html_soup = BeautifulSoup(html_string, "html.parser")
     image_srcs = []
     for image_tag in html_soup.find_all("img"):
         image_src = image_tag["src"]
-        if image_src.startswith('data:image/gif;base64'):
+        if image_src.startswith('data:image/gif;base64') | image_src.startswith('data:image/png;base64'):
             continue
         if image_src.startswith('//'):
             image_src = image_src[2:]
@@ -55,10 +55,6 @@ def get_image_srcs(html_string, url):
             image_src = image_src[1:]
         image_srcs.append(image_src)
     
-    # div_style = html_soup.find('div')['style']
-    # style = cssutils.parseStyle(div_style)
-    # url = style['background-image']
-    # print(url)
     return image_srcs
 
 def get_a_hrefs(html_string):
