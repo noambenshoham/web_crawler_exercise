e65e0c975a336141dece043350c6a24433ed7214
noambenshoham <noambenshoham@cyber4s.dev>
2022-11-23 04:33:34 +0200

    Getting background images

--- web_crawler.py
+++ web_crawler.py
@@ -11,11 +11,14 @@ def main():
     main_url = sys.argv[1]
     depth_string = sys.argv[2]
     results = get_results(main_url, 0, int(depth_string))
- 
-    results_json = {"results": results}
-    result_file_path = Path("results.json")
-    results_json_string = json.dumps(results_json)
-    result_file_path.write_text(results_json_string)
+    
+
+    
+    
+    # results_json = {"results": results}
+    # result_file_path = Path("results.json")
+    # results_json_string = json.dumps(results_json)
+    # result_file_path.write_text(results_json_string)
 
 def get_results(url, depth, max_depth):
     results = []
@@ -35,25 +38,43 @@ def get_results(url, depth, max_depth):
     links = get_a_hrefs(html_string)
     for link in links:
         results += get_results(link, depth + 1, max_depth)
+        # print(results)
     return results
 
 def get_web_page(page_link):
     response = requests.get(page_link)
     file_content = response.text
     return file_content
+
+def get_bg_images(html_string, url):
+    bg_images = []
+    soup = BeautifulSoup(html_string, "html.parser")
+    for items in soup.select("[style*='background-image']"):
+        # image= "https:" + items['style'].split("url(")[1].split(")")[0]
+        # print(image)
+        style_attrs = items.get('style').split(";")
+        for style_attr in style_attrs:
+            if style_attr.startswith('background'):
+                bg_images.append(url +'/' + style_attr[22:-2])
+    return bg_images
+
 def get_image_srcs(html_string, url):
 
     html_soup = BeautifulSoup(html_string, "html.parser")
     image_srcs = []
     for image_tag in html_soup.find_all("img"):
         image_src = image_tag["src"]
-        if image_src.startswith('data:image/gif;base64') | image_src.startswith('data:image/png;base64'):
+        if image_src.startswith('data:image/gif;base64') or image_src.startswith('data:image/png;base64'):
             continue
         if image_src.startswith('//'):
             image_src = image_src[2:]
         elif image_src.startswith('/'):
             image_src = image_src[1:]
         image_srcs.append(image_src)
+        
+    for bg_image in get_bg_images(html_string, url):
+        if (validators.url(bg_image) == True):
+            image_src += bg_image
     
     return image_srcs
 
